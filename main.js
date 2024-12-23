e8656b1bbc64c68b5cf0fe95cc380e6da9d69276
saif0110 <khanmohdtauheed4@gmail.com>
2024-12-23 12:23:16 +0530

    file changed

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 const http = require('http');
+const fs = require('fs');
 
 // TODO: Create an HTTP server
+const server = http.createServer((req, res)=>{
+    fs.readFile('output.txt', (err, data)=>{
+        if(err){
+            console.log(err);
+        }else{
+            console.log(data.toString());
+        }
+    })
+});
 
 // TODO: Listen on port 3000
+server.listen(3000, ()=>{
+    console.log('server listening on port number 3000');
+})
 
 // DO NOT EDIT BELOW THIS LINE (Uncomment it once you are done with your code)
 
